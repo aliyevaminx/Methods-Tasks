51712f5badfffa6d22f354d059af33d25130d6e9
am1nll <aminsha@code.edu.az>
2024-05-05 20:32:47 +0400

    Files uploaded

---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////////////////////////////TASK1
+
+            //Console.WriteLine("1. ededi daxil edin: ");
+            //int input1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("2. ededi daxil edin: ");
+            //int input2 = Convert.ToInt32(Console.ReadLine());
+
+            //FindResult(input1, input2);
+
+
+            /////////////////////////////////////////////////////////////////TASK2
+
+            //Console.WriteLine("Ededi daxil edin: ");
+            //int input = Convert.ToInt32(Console.ReadLine());
+
+            //FindResult(input);
+
+
+            /////////////////////////////////////////////////////////////////TASK3
+
+            //int[] arr = { 2425, 274, 8424, 572, 215, 51, 683 };
+            //FindSum(arr);
+
+
+            /////////////////////////////////////////////////////////////////TASK4
+
+            //SendWelcomeMessage("Amin", "Eliyev");
+
+
+            /////////////////////////////////////////////////////////////////TASK5
+
+            //Console.WriteLine(ReverseString(Console.ReadLine()));
+
+            /////////////////////////////////////////////////////////////////TASK6
+
+            //CheckNumber(Convert.ToInt32(Console.ReadLine()));
+
+            /////////////////////////////////////////////////////////////////TASK7
+
+            //ShowResult(2, 4, 5);
+
+            /////////////////////////////////////////////////////////////////TASK11
+            //CountOfSpace(Console.ReadLine());
+        }
+
+
+
+        /////////////////////////////////////////////////////////////////TASK1 Method
+
+        //public static void FindResult(int number1, int number2)
+        //{
+        //    int result = number1 * number2;
+        //    Console.WriteLine(result);
+        //}
+
+
+        /////////////////////////////////////////////////////////////////TASK2 Method
+
+        //public static void FindResult(int input)
+        //{
+        //    int result = input * input;
+        //    Console.WriteLine(result);
+        //}
+
+
+        /////////////////////////////////////////////////////////////////TASK3 Method
+
+        //public static void FindSum(int[] arr)
+        //{
+        //    int sum = 0;
+
+        //    foreach (int arrItem in arr)
+        //    {
+        //        sum += arrItem;
+        //    }
+        //    Console.WriteLine(sum);
+        //}
+
+
+        /////////////////////////////////////////////////////////////////TASK4 Method
+
+        //public static void SendWelcomeMessage(string name, string surname)
+        //{
+        //    Console.WriteLine($"{name} {surname}, xos gelmisiz...");
+        //}
+
+
+        /////////////////////////////////////////////////////////////////TASK5 Method
+
+        //public static string ReverseString(string word)
+        //{
+        //    string reversedString = string.Empty;
+
+        //    for (int i = word.Length - 1; i >= 0; i--)
+        //    {
+        //        reversedString += word[i];
+        //    }
+
+        //    return reversedString;
+        //}
+
+        /////////////////////////////////////////////////////////////////TASK6 Method
+
+        //public static void CheckNumber(int number)
+        //{
+        //    int temp;
+
+        //    if (number % 7 == 0)
+        //    {
+        //        Console.WriteLine(number);
+        //    } else
+        //    {
+        //        temp = number % 7;
+        //        number = number - temp;
+        //        Console.WriteLine(number);
+        //    }
+        //}
+
+        /////////////////////////////////////////////////////////////////TASK7 Method
+
+        //public static void ShowResult (int a, int b, int c)
+        //{
+        //    Console.WriteLine(a + b + c);
+        //}
+
+        //public static void ShowResult(int a)
+        //{
+        //    Console.WriteLine(a * a);
+        //}
+
+        /////////////////////////////////////////////////////////////////TASK11 Method
+
+        //public static void CountOfSpace(string sentence)
+        //{
+
+        //    int count = 0;
+
+        //    foreach (var sentenceItem in sentence)
+        //    {
+        //        if (sentenceItem == ' ')
+        //        {
+        //            count++;
+        //        }
+        //    }
+
+        //    Console.WriteLine(count);
+        //}
+    }
+}
